25d45942ddc8acedb1bfad8b69240281bce81d0b
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-29 01:13:41 +0100

    Larapi updated

--- config/larapi.php
+++ config/larapi.php
@@ -17,5 +17,10 @@ return [
     'extra_routes_namespaces' => [
         'Api' => base_path() . DIRECTORY_SEPARATOR . 'api',
         'Infrastructure' => base_path() . DIRECTORY_SEPARATOR . 'infrastructure'
+    ],
+
+    'exceptions_formatters' => [
+        Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException::class => one2tek\larapi\ExceptionsFormatters\UnprocessableEntityHttpExceptionFormatter::class,
+        Throwable::class => one2tek\larapi\ExceptionsFormatters\ExceptionFormatter::class
     ]
 ];
