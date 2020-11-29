a04512b7cc240a8d67a1efe2b31bee9856b81812
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-29 11:21:11 +0100

    Improved AccessTokenChecker

--- infrastructure/Http/Middlewares/AccessTokenChecker.php
+++ infrastructure/Http/Middlewares/AccessTokenChecker.php
@@ -3,35 +3,28 @@
 namespace Infrastructure\Http\Middlewares;
 
 use Closure;
-use Illuminate\Foundation\Application;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Auth\AuthenticationException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class AccessTokenChecker
 {
-    private $app;
-
-    private $oAuthMiddleware;
+    private $authenticate;
 
     public function __construct(
-        Application $app,
         Authenticate $authenticate
     ) {
-        $this->app = $app;
         $this->authenticate = $authenticate;
     }
 
     public function handle($request, Closure $next, $scopesString = null)
     {
-        if ($this->app->environment() !== 'testing') {
-            try {
-                return $this->authenticate->handle($request, $next, 'api');
-            } catch (AuthenticationException $e) {
-                throw new UnauthorizedHttpException('Challenge');
-            }
+        try {
+            return $this->authenticate->handle($request, $next, 'api');
+        } catch (AuthenticationException $e) {
+            throw new UnauthorizedHttpException('Challenge', 'Login to continue.');
         }
-
+ 
         return $next($request);
     }
 }
