bd0da0c81b3a9d9dd41ed6f46b0eaafa0026c6db
Gentrit Abazi <35135482+gentritabazi01@users.noreply.github.com>
2020-12-29 19:38:21 +0100

    Update and rename larapi-components.php to larapi.php

---
+++ config/larapi.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'modules_folder' => 'api',
+
+    'extra_routes' => [
+        'routes' => [
+            'middleware' => ['auth:api'],
+            'namespace' => 'Controllers',
+            'prefix' => null
+        ],
+        'routes_public' => [
+            'middleware' => [],
+            'namespace' => 'Controllers',
+            'prefix' => null
+        ],
+    ],
+
+    'extra_routes_namespaces' => [
+        'Api' => base_path() . DIRECTORY_SEPARATOR . 'api',
+        'Infrastructure' => base_path() . DIRECTORY_SEPARATOR . 'infrastructure'
+    ],
+
+    'exceptions_formatters' => [
+        Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException::class => one2tek\larapi\ExceptionsFormatters\UnprocessableEntityHttpExceptionFormatter::class,
+        Throwable::class => one2tek\larapi\ExceptionsFormatters\ExceptionFormatter::class
+    ]
+    
+];
