bfa054d160bc0b52bed5ca413bc61d7c24383151
Gentrit Abazi <35135482+gentritabazi01@users.noreply.github.com>
2020-12-30 22:18:54 +0100

    Update Repository.php

--- infrastructure/Abstracts/Repository.php
+++ infrastructure/Abstracts/Repository.php
@@ -6,5 +6,7 @@ use one2tek\larapi\Database\Repository as BaseRepository;
 
 abstract class Repository extends BaseRepository
 {
-    //
+    protected $sortProperty = 'id';
+
+    protected $sortDirection = 'DESC';
 }
