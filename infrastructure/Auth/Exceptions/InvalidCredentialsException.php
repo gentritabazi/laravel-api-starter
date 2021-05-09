4dd49cc7f64cab809cb14e6fb94462c29171663b
Gentrit Abazi <gentritabazi01@gmail.com>
2021-05-09 20:59:32 +0200

    Improve InvalidCredentialsException.

--- infrastructure/Auth/Exceptions/InvalidCredentialsException.php
+++ infrastructure/Auth/Exceptions/InvalidCredentialsException.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class InvalidCredentialsException extends UnauthorizedHttpException
 {
-    public function __construct($message = null, \Exception $previous = null, $code = 0)
+    public function __construct($message = 'Invalid Credentials!', \Exception $previous = null, $code = 0)
     {
         parent::__construct('', $message, $previous, $code);
     }
