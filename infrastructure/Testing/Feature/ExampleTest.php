b8401038f6a4fb402458a519b8af5f6a9072a28c
Gentrit Abazi <gentritabazi01@gmail.com>
2021-06-06 18:55:46 +0200

    Improve tests.

---
+++ infrastructure/Testing/Feature/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Infrastructure\Testing\Feature;
+
+use Infrastructure\Testing\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
